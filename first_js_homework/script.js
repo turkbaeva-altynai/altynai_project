5b819eb09074430dcc9f5a1c9c8db1b343395689
Altynai98 <altynaiermekovna98@gmail.com>
2025-02-17 18:11:18 +0200

    Create first js code

---
+++ first_js_homework/script.js
@@ -0,0 +1,11 @@
+let country = "Germany";
+let continent = "Europe";
+let population = 84.5;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isISland = false;
+countryDescription = `${country} is located in ${continent}, ${population} million people live here.`;
+console.log(countryDescription);
